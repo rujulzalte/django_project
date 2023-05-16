c8281f94926d04b252cc477daadc4bc2349a6f15
ERUDITE-Labhesh <labhesh.l@consultadd.com>
2023-05-16 11:52:18 -0400

    Worked on Models and Admin Functionalilty

--- Login/admin.py
+++ Login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import UserLogin
 # Register your models here.
+
+
+# admin.site.register(UserLogin)
+
+@admin.register(UserLogin)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'username', 'password', 'Phone_no', 'state']
--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class UserLogin(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=10)
+    Phone_no = models.IntegerField()
+    state = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.username}"
+
